9994c4ef1ede9e1951f026c68eb76cfd40b87881
LarsG <lars.gunnar@jobloop.no>
2023-06-20 10:49:08 +0000

    add navbar internal links

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,10 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function Layout() {
   return (
     <div>
       <header>
         <h2>Header</h2>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
 
       <main>
